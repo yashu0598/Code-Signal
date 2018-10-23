14f2f6cc744479beefbab9239dbc357423a50fcc
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-10-23 21:56:33 +0530

    Create findTheRemainder.java

---
+++ findTheRemainder.java
@@ -0,0 +1,13 @@
+/*
+
+Given two integers a and b, find the remainder of a when divided by b.
+
+Example
+
+For a = 5 and b = 3, the output should be
+findTheRemainder(a, b) = 2.
+*/
+
+int findTheRemainder(int a, int b) {
+return a%b;
+}
