a007b800704a5f1dbfbf36e71a01459edc46f945
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-10-23 21:43:45 +0530

    Create returnTwelve.java

---
+++ returnTwelve.java
@@ -0,0 +1,15 @@
+/*
+
+If the given integer is greater than 12 return it, otherwise return 12.
+
+Example
+
+For number = 13, the output should be
+returnTwelve(number) = 13;
+For number = 10, the output should be
+returnTwelve(number) = 12.
+*/
+
+int returnTwelve(int number) {
+    return (number>12)?number:12;
+}
