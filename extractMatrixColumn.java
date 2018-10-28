2334ac24824d1ce0b9aa3fc52ec46f1c74f45d56
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-10-28 20:11:33 +0530

    Create extractMatrixColumn.java

---
+++ extractMatrixColumn.java
@@ -0,0 +1,19 @@
+/*
+Given a rectangular matrix and an integer column, return an array containing the elements of the columnth column of the given matrix (the leftmost column is the 0th one).
+
+Example
+
+For
+
+matrix = [[1, 1, 1, 2], 
+          [0, 5, 0, 4], 
+          [2, 1, 3, 6]]
+and column = 2, the output should be
+extractMatrixColumn(matrix, column) = [1, 0, 3].
+*/
+int[] extractMatrixColumn(int[][] matrix, int column) {
+int a[]=new int[matrix.length];
+    for(int i=0;i<matrix.length;i++)
+        a[i]=matrix[i][column];
+        return a;
+}
