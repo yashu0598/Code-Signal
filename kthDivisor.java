70a62099331f42369e0ea658bc89fe5237baed99
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-11-03 15:40:51 +0530

    Update kthDivisor.java

--- kthDivisor.java
+++ kthDivisor.java
@@ -1,23 +1,5 @@
-int kthDivisor(int n, int k) {
-  ArrayList<Integer> divisors = new ArrayList<>();
-  int i = 1;
-  while (i * i < n) {
-    if (n % i == 0) {
-      divisors.add(i);
-    }
-    i++;
-  }
-  if (i * i > n) {
-    i--;
-  }
-  while (i > 0) {
-    if (n % i == 0) {
-      divisors.add(n / i);
-    }
-    i--;
-  }
-  if (divisors.size() <= k) {
-    return -1;
-  }
-  return divisors.get(k - 1);
+int kthDigit(int n, int k) {
+    if(k>(""+n).length())
+        return -1;
+    return (""+n).charAt(k-1)-'0';
 }
