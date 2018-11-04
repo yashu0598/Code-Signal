092ae353ee0459e00fae29616fff6cef8431e0d1
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-11-04 14:07:14 +0530

    Update arraySumAdjacentDifference.java

--- arraySumAdjacentDifference.java
+++ arraySumAdjacentDifference.java
@@ -1,6 +1,6 @@
 int arraySumAdjacentDifference(int[] inputArray) {
     int m=0;
-    for(int i=0;i<inputArray.length-abs;i++)
+    for(int i=0;i<inputArray.length-1;i++)
         m+=Math.abs(inputArray[i]-inputArray[i+1]);
     return m;
 }
