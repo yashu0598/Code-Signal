9523a48644a8f3cd894cd3d1acbda1eecd0d9564
Yashwanth Chowdary Kota <kotayashwanthchowdary52@gmail.com>
2018-11-10 19:25:38 +0530

    Create countWays.java

---
+++ countWays.java
@@ -0,0 +1,26 @@
+int countWays(int n, int k) {
+  final int MOD = (int)1e9 + 7;
+
+  ArrayList<ArrayList<Integer>> c_nk = new ArrayList<>();
+  for (int i = 0; i <= n; i++) {
+    c_nk.add(new ArrayList<Integer>());
+    for (int j = 0; j <= k; j++) {
+      c_nk.get(i).add(0);
+    }
+  }
+  c_nk.get(0).set(0, 1);
+  for (int i = 0; i < n; i++) {
+    ArrayList<Integer> cur = c_nk.get(i);
+    ArrayList<Integer> next = c_nk.get(i + 1);
+    for (int j = 0; j <= k; j++) {
+      if (i + 1 <= n) {
+        next.set(j, (next.get(j) + cur.get(j)) % MOD);
+        if (j + 1 <= k) {
+          next.set(j + 1, (next.get(j + 1) + cur.get(j)) % MOD);
+        }
+      }
+    }
+  }
+
+  return c_nk.get(n).get(k);
+}
